4e78f4c9b86d1264cb287e13877bb76db8a48247
EchoHunter <48270082+EchoHunter@users.noreply.github.com>
2021-06-17 18:30:43 -0700

    :b:
    finished array cheat sheet and partially did robot race, im stuck on robot race though, can't get robots to move

--- src/_00_IntroToArrays/_01_RobotRace.java
+++ src/_00_IntroToArrays/_01_RobotRace.java
@@ -8,16 +8,27 @@ import org.jointheleague.graphical.robot.Robot;
 
 public class _01_RobotRace {
 	//1. make a main method
+	public static void main(String[] args) {
+		Random rand = new Random();
 	
 		//2. create an array of 5 robots.
-
+Robot[] r = new Robot[5];
 		//3. use a for loop to initialize the robots.
-
+int separation = 0;
+for(Robot v: r) {
+	v = new Robot();
+	v.moveTo(50 + separation,500);
+	separation +=200;
+}
 			//4. make each robot start at the bottom of the screen, side by side, facing up
-	
+
 		//5. use another for loop to iterate through the array and make each robot move 
 	    //   a random amount less than 50.
-    	
+//Boolean notFinished = true;
+for (int i = 0; i < r.length; i++) {
+r[i].move(rand.nextInt(50));	
+}
+
 		//6. use a while loop to repeat step 5 until a robot has reached the top of the screen.
 
 		//7. declare that robot the winner and throw it a party!
@@ -26,5 +37,5 @@ public class _01_RobotRace {
     	
 	    //9. make the robots race around a circular track.
 
-	
+	}
 }
